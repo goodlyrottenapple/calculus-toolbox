b9684f64e66eed79ba7ad666271d0a37f880c6a5
Samuel Balco <goodlyrottenapple@gmail.com>
2017-10-24 10:35:19 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 docker build -t calculus-toolbox .
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
-	xhost + $ip
-	docker run -ti -v ${PWD}/calculi:/root/calculi -e DISPLAY=$ip:0 calculus-toolbox
+    #ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+    #xhost + $ip
+    docker run -ti -v ${PWD}/calculi:/root/calculi calculus-toolbox
 else
     docker run -ti -v ${PWD}/calculi:/root/calculi -e DISPLAY=$DISPLAY calculus-toolbox
 fi
